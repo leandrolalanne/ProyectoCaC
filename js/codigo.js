9dfb3caaecbc4dc4d7450e4b80805ceeba77468a
dario923 <sanajani410@gmail.com>
2023-05-21 10:46:13 -0300

    corregi codigo css js

--- js/codigo.js
+++ js/codigo.js
@@ -1,8 +1,8 @@
 console.log("se carga el codigo");
 window.addEventListener("DOMContentLoaded", (event)=>{
     console.log("DOM completamente cargado y procesado");
-    const menuBtn= document.getElementById("header div.collapse navbar-collapse nav justify-content-end");
-    const nav= document.querySelector("header nav");
+    const menuBtn= document.getElementById("header button.navbar-toggler");
+    const nav= document.querySelector("header.header sticky-top");
     const body=document.querySelector("body");
 
     menuBtn.addEventListener("click", (event) =>{
