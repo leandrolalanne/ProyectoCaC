c77d4658a82682d42da31d364529852769bfe7de
dario923 <sanajani410@gmail.com>
2023-05-30 22:36:12 -0300

    agregue codigo

--- js/codigo.js
+++ js/codigo.js
@@ -3,24 +3,19 @@ window.addEventListener("DOMContentLoaded",(event)=>{
     console.log("DOM completamente cargado y procesado");
 
     const menuBtn=document.getElementById("myDIV");
-    const reservado=document.getElementById("reservado");
+    
     
 
     menuBtn.addEventListener("click", (event)=>{
         var x = document.getElementById("myDIV");
+
+        /*const x = document.getElementById("myDIV");  */
             if (x.innerHTML === "Enviar mensaje") {
                 x.innerHTML = "Mensaje enviado";
             } else {
                 x.innerHTML = "Enviar mensaje";
             }
     })
-    reservado.addEventListener("click", (event)=>{
-        var x = document.getElementById("reservado");
-            if (x.innerHTML === "RESERVAR") {
-                x.innerHTML = "RESERVADO";
-            } else {
-                x.innerHTML = "RESERVAR";
-            }
-    })        
+    
 
 })
